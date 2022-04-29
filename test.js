4e4ccb3a4f2e3a9914a322a6e1a9f0d0061cd005
maruf <maruf123erkinov@gmail.com>
2022-04-29 17:32:46 +0500

    added js file and modified

---
+++ test.js
@@ -0,0 +1,31 @@
+// let CarName = Volvo;
+//let x = 50 ;
+// let x = 5 ;
+// let y = 10;
+// console.log(x*y);
+// let z = alert(x+y);
+
+//let name = "John"; //str, surname = 'Doe' // str ,age = 35; //num
+
+// const x = {
+//     firstName = "John";
+//     lastName = 'Doe';
+
+// } //str
+
+
+// function myFunction (){
+//     alert('hello world');
+// }
+// myFunction();
+// for(var i = 1; i <= 10; i++){
+//        document.write(i);
+//     }
+
+// let button1 = document.querySelector('button');
+
+// button1.onclick=function(){
+//     button1.style.background ='red';
+//     button1.innerHTML=('salom');
+
+// }
