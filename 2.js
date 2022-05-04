3877d4ef939cd73a025d44096cb6a213d3162ad8
maruf <maruf123erkinov@gmail.com>
2022-05-04 13:23:07 +0500

    2days js file added

---
+++ 2.js
@@ -0,0 +1,26 @@
+// let button1 = document.querySelector('button');
+// let button1 = document.querySelector(".but_class");
+// let button1 = document.querySelector("#but_id");
+// let h11 = document.querySelector("h1");
+
+
+// button1.onclick = function (){
+//     // console.log(button1);
+//     // document.write('salom');
+//     button1.style.background="red";
+//     // button1.style.color='yellow';
+//     button1.innerHTML='hehe';
+//     h11.style.color='red';
+// }
+
+// button1.ondblclick = function(){
+//     button1.style.background='blue';
+//     h11.style.color='blue';
+// }
+
+
+// let i = 1;
+// while (i <= 10) {
+//     alert(i);
+//     ++i;
+// }
